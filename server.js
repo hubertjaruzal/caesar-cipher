189b97e093cecc4d884865babceb638d81ebb65c
hubertjaruzal <hubertjaruzal@gmail.com>
2016-04-25 20:41:31 +0200

    initial

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-const fallback = require('express-history-api-fallback');
-const express = require('express');
-const app = express();
+var express = require('express');
+var path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  if (process.env.BASIC_AUTH_USERNAME || process.env.BASIC_AUTH_PASSWORD) {
-    const basicAuth = require('basic-auth');
-    const auth = (req, res, next) => {
-      function unauthorized() {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-        return res.sendStatus(401);
-      }
+var app = express();
 
-      const user = basicAuth(req);
-      if (!user || !user.name || !user.pass) {
-        return unauthorized(res);
-      }
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+var publicPath = path.resolve(__dirname, 'views');
 
-      if (user.name === process.env.BASIC_AUTH_USERNAME && user.pass === process.env.BASIC_AUTH_PASSWORD) {
-        return next();
-      } else {
-        return unauthorized();
-      }
-    };
+// We point to our static assets
+app.use(express.static(publicPath));
 
-    app.use(auth);
-  }
-}
-
-const root = `${__dirname}/dist`;
-app.use(express.static(root));
-app.use(fallback('index.html', { root }));
-app.get('/', (request, response) => response.render('dist/index.html'));
-
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), () => console.log('Sellektor Admin (production) is running on port', app.get('port')));
+// And run the server
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
+});
