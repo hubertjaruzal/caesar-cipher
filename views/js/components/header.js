97dfa06c543069209674598c7bde871a8379f808
hubertjaruzal <hubertjaruzal@gmail.com>
2016-05-10 19:16:49 +0200

    RWD

--- views/js/components/header.js
+++ views/js/components/header.js
@@ -4,22 +4,12 @@ import { Link } from "react-router";
 
 export default class Header extends React.Component {
   render() {
-    const navStyle = {
-      listStyleType: "none"
-    };
-    const itemStyle = {
-      textDecoration: "none",
-      fontSize: "15px",
-      color: "black"
-    }
-
-
     return(
       <nav class="nav">
-        <ul style={navStyle}>
-          <li><Link to="decryption" activeClassName="active" style={itemStyle}>Decryption</Link></li>
-          <li><Link to="encryption" activeClassName="active" style={itemStyle}>Encryption</Link></li>
-          <li><Link to="about" activeClassName="active" style={itemStyle}>About Cipher</Link></li>
+        <ul>
+          <li><Link to="decryption" activeClassName="active">Decryption</Link></li>
+          <li><Link to="encryption" activeClassName="active">Encryption</Link></li>
+          <li><Link to="about" activeClassName="active">About Cipher</Link></li>
         </ul>
       </nav>
     );
