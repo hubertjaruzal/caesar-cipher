d7a5b8c1e989c91e95bac0ba51fd87f915f19dc4
hubertjaruzal <hubertjaruzal@gmail.com>
2016-05-11 13:37:40 +0200

    Render

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var path = require('path');
 var fallback = require('express-history-api-fallback');
+
 import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from './views/js/application.js'
+
+serve((req, res) => {
+  // Note that req.url here should be the full URL path from
+  // the original request, including the query string.
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      // You can also check renderProps.components or renderProps.routes for
+      // your "not found" component or route respectively, and send a 404 as
+      // below, if you're using a catch-all route.
+      res.status(200).send(renderToString(<RouterContext {...renderProps} />))
+    } else {
+      res.status(404).send('Not found')
+    }
+  })
+})
+});
 
 var app = express();
 
@@ -12,32 +35,6 @@ var publicPath = path.resolve(__dirname, 'views');
 // We point to our static assets
 app.use(express.static(publicPath));
 app.use(fallback('index.html', { root: publicPath }))
-
-const routes = require('../jsx/routes');
-const React = require('react');
-const {RoutingContext, match} = require('react-router');
-const hist = require('history');
-
-app.use((req, res, next) => {
-  const location = hist.createLocation(req.path);
-  match({
-    routes: routes,
-    location: location,
-  }, (err, redirectLocation, renderProps) => {
-    if (redirectLocation) {
-      res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-    } else if (err) {
-      console.log(err);
-      next(err);
-      // res.send(500, error.message);
-    } else if (renderProps === null) {
-      res.status(404)
-        .send('Not found');
-    } else {
-      res.send('<!DOCTYPE html>' + React.renderToString(<RoutingContext {...renderProps}/>));
-    }
-  });
-});
 
 // And run the server
 app.listen(port, function () {
