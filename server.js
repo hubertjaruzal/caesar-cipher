c4fa68b3391107123de23061e0ffa550e561490b
hubertjaruzal <hubertjaruzal@gmail.com>
2016-05-11 15:19:13 +0200

    Render

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var path = require('path');
 var fallback = require('express-history-api-fallback');
-var ReactEngine = require('express-react-engine');
+var React = require('react');
+var Router = require('react-router');
 
 var app = express();
 
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, 'views');
-
-
-app.engine('jsx', ReactEngine());
-
 // We point to our static assets
 app.use(express.static(publicPath));
-app.use(fallback('index.html', { root: publicPath }));
+app.get('/favicon.ico', (req, res) => res.send(''));
+app.use((req, res) => {
+  Router.run(routes, req.path, (Handler) => {
+    res.send('<!DOCTYPE html>' + React.renderToString(<Handler path={req.path} />));
+  });
+});
+
 
 // And run the server
 app.listen(port, function () {
