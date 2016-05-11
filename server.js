d2901b6ee342b5a86e27d901fce7d40c0d684021
hubertjaruzal <hubertjaruzal@gmail.com>
2016-05-11 13:49:04 +0200

    Render

--- server.js
+++ server.js
@@ -2,30 +2,6 @@ var express = require('express');
 var path = require('path');
 var fallback = require('express-history-api-fallback');
 
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from './views/js/application.js'
-
-serve((req, res) => {
-  // Note that req.url here should be the full URL path from
-  // the original request, including the query string.
-  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message)
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      // You can also check renderProps.components or renderProps.routes for
-      // your "not found" component or route respectively, and send a 404 as
-      // below, if you're using a catch-all route.
-      res.status(200).send(renderToString(<RouterContext {...renderProps} />))
-    } else {
-      res.status(404).send('Not found')
-    }
-  })
-})
-});
-
 var app = express();
 
 var isProduction = process.env.NODE_ENV === 'production';
@@ -40,3 +16,20 @@ app.use(fallback('index.html', { root: publicPath }))
 app.listen(port, function () {
   console.log('Server running on port ' + port);
 });
+
+
+var React = require('react'),
+ReactApp = React.createFactory(require('../views/js/application'));
+
+module.exports = function(app) {
+
+  app.get('/', function(req, res){
+
+    // React.renderToString takes your component and generates the markup
+    var reactHtml = React.renderToString(ReactApp({}));
+
+    // Output html rendered by react into .ejs file. Can be any template
+    res.render('index.ejs', {reactOutput: reactHtml});
+  });
+
+};
